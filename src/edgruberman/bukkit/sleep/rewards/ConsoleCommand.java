c844880c0c24af371465bd3f9f6dd9aa92c8d104
EdGruberman <ed@rjump.com>
2012-08-01 15:40:38 -0700

    CHG: Added ConsoleCommand reward

---
+++ src/edgruberman/bukkit/sleep/rewards/ConsoleCommand.java
@@ -0,0 +1,35 @@
+package edgruberman.bukkit.sleep.rewards;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import edgruberman.bukkit.sleep.Main;
+
+public class ConsoleCommand extends Reward {
+
+    public String format;
+    public int value;
+
+    @Override
+    public Reward load(final ConfigurationSection definition) {
+        super.load(definition);
+        this.format = definition.getString("format");
+        this.value = definition.getInt("value");
+        return this;
+    }
+
+    @Override
+    public void apply(final Player player, final int participants) {
+        final int result = (int) (this.value * this.factorFor(participants));
+        final String command = String.format(this.format, player.getName(), result);
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
+        Main.messenger.plugin.getLogger().finest("Rewarded " + player.getName() + " by dispatching console command \"" + command + "\"");
+    }
+
+    @Override
+    public String toString() {
+        return "Command = name: \"" + this.name + "\", format: \"" + this.format + "\", value: " + this.value + ", factor: " + Reward.DECIMAL_FORMAT.format(this.factor);
+    }
+
+}
