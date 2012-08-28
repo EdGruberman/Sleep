b8387044043725e61204f63039988f0389d55870
EdGruberman <ed@rjump.com>
2012-08-27 20:38:33 -0700

    FIX: No notification leaving bed after going idle in bed

--- src/edgruberman/bukkit/sleep/Main.java
+++ src/edgruberman/bukkit/sleep/Main.java
@@ -23,7 +23,7 @@ public final class Main extends CustomPlugin {
     @Override
     public void onLoad() {
         final DependencyManager dm = new DependencyManager(this);
-        if (!dm.isValidPlugin("PlayerActivity", "edgruberman.bukkit.playeractivity", "3.0.0b48")) {
+        if (!dm.isValidPlugin("PlayerActivity", "edgruberman.bukkit.playeractivity", "3.0.0b49")) {
             if (Bukkit.getPluginManager().getPlugin("PlayerActivity") != null) {
                 this.getLogger().severe("Outdated PlayerActivity plugin;  Stop server, delete \"plugins/PlayerActivity.jar\", and then restart server");
                 throw new IllegalStateException("PlayerActivity plugin outdated");
