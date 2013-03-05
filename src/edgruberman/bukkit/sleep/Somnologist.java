3765f86252f69a146aae0a4bc13c2ec287a59021
EdGruberman <ed@rjump.com>
2013-03-05 10:59:45 -0700

    Clarify logging

--- src/edgruberman/bukkit/sleep/Somnologist.java
+++ src/edgruberman/bukkit/sleep/Somnologist.java
@@ -46,12 +46,12 @@ public final class Somnologist implements Listener {
     /** create state based on configuration */
     State loadState(final World world) {
         if (world.getEnvironment() != Environment.NORMAL) {
-            this.plugin.getLogger().log(Level.CONFIG, "[{0}] Sleep state tracking excluded because environment is {1}", new Object[] { world.getName(), world.getEnvironment() });
+            this.plugin.getLogger().log(Level.CONFIG, "[{0}] World sleep state tracking excluded because environment is {1}", new Object[] { world.getName(), world.getEnvironment() });
             return null;
         }
 
         if (this.excluded.contains(world.getName())) {
-            this.plugin.getLogger().log(Level.CONFIG, "[{0}] Sleep state tracking explicitly excluded", world.getName());
+            this.plugin.getLogger().log(Level.CONFIG, "[{0}] World sleep state tracking explicitly excluded", world.getName());
             return null;
         }
 
