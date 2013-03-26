ae0a459abf8ec347241cee56c35ef7b24fec1434
EdGruberman <ed@rjump.com>
2013-03-26 00:01:08 -0700

    Add CraftBukkit 1.5.1 support

---
+++ src/edgruberman/bukkit/sleep/craftbukkit/CraftBukkit_v1_5_R2.java
@@ -0,0 +1,36 @@
+package edgruberman.bukkit.sleep.craftbukkit;
+
+import net.minecraft.server.v1_5_R2.ChunkCoordinates;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_5_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class CraftBukkit_v1_5_R2 extends CraftBukkit {
+
+    @Override
+    public void bedEject(final Player player) {
+        final CraftPlayer cp = (CraftPlayer) player;
+        cp.getHandle().a(true, true, true); // reset sleep ticks, update sleeper status for world, set as current bed spawn
+    }
+
+    @Override
+    public Location getBed(final Player player) {
+        final CraftPlayer cp = (CraftPlayer) player;
+
+        final World world = Bukkit.getServer().getWorld(cp.getHandle().spawnWorld);
+        if (world == null) return null;
+
+        final ChunkCoordinates bed = cp.getHandle().getBed();
+        if (bed == null) return null;
+
+        final int id = ( cp.getHandle().isRespawnForced() ? Material.BED_BLOCK.getId() : CraftBukkit.blockTypeId(world, bed.x, bed.y, bed.z) );
+        if (id != Material.BED_BLOCK.getId()) return null;
+
+        return new Location(world, bed.x, bed.y, bed.z);
+    }
+
+}
