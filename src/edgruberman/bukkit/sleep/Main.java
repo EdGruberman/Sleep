740c20b85744a10eb4062c11ad01d8c0cf9bcc18
EdGruberman <ed@rjump.com>
2013-03-25 23:26:56 -0700

    Update PlayerActivity to v4.2.0

--- src/edgruberman/bukkit/sleep/Main.java
+++ src/edgruberman/bukkit/sleep/Main.java
@@ -44,7 +44,7 @@ public final class Main extends CustomPlugin {
         this.putConfigMinimum("7.0.0b21");
         this.putConfigMinimum(Main.LANGUAGE_FILE, "7.0.0b21");
 
-        final PluginDependency dependency = new PluginDependency(this, "PlayerActivity", "edgruberman.bukkit.playeractivity", "4.1.2");
+        final PluginDependency dependency = new PluginDependency(this, "PlayerActivity", "edgruberman.bukkit.playeractivity", "4.2.0");
         if (dependency.isValid()) {
             this.loaded = true;
             return;
